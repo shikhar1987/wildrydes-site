61d79e3974ecf053ea4da2a7c4c28e5443da4b1e
Shikhar <shikhar_sharma@live.com>
2022-08-11 14:41:39 +1000

    updated js with cognito details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_ScMYMEl2k', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'n6ssr7bt0qkbpjac7t88osn8r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
